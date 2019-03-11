22dd80e92bb879953737025776b116933fa32e7a
Aziz Zainutdin <aloha@scriptor.me>
2019-03-11 20:50:43 +0300

    Small details

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,18 @@
 <template lang="pug">
-    section.container
-        div
-            logo/
-            h1.title.is-1 vuemastery
-            h2.subtitle.is-2 My striking Nuxt.js project
-            div.links
-                a.button.is-primary(
-                    href="https://nuxtjs.org/"
-                    target="_blank"
-                ) Documentation
-                |&nbsp;
-                a.button(
-                    href="https://github.com/nuxt/nuxt.js"
-                    target="_blank"
-                ) GitHub
+    div
+        logo/
+        h1.title.is-1 vuemastery
+        h2.subtitle.is-2 My striking Nuxt.js project
+        div.links
+            a.button.is-primary(
+                href="https://nuxtjs.org/"
+                target="_blank"
+            ) Documentation
+            |&nbsp;
+            a.button(
+                href="https://github.com/nuxt/nuxt.js"
+                target="_blank"
+            ) GitHub
 </template>
 
 <script>
